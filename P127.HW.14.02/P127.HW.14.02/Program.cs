1fad174d45363443b53a12470cc65dd6c32f0c39
RaminRzayev <raminag@code.edu.az>
2022-02-15 04:31:58 +0400

    add HW

---
+++ P127.HW.14.02/P127.HW.14.02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P127.HW._14._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 70, 65, 100, 90, 30, 95, 40 };
+        }
+        public static int firstMethod(int[] points,ref int SumStudy)
+        {
+            int sum = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                
+                sum += points[i];
+                
+            }
+            return sum;
+        }
+        public static int secondMethod(int SumPoints,ref int SumStudy)
+        {
+            return SumPoints / SumStudy;
+        }
+    }
+}
